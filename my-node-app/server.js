6d9d2f09dfacd6a9336aacc885c6b9d147f53b53
kelkaal <kelechikalu95@gmail.com>
2025-05-26 13:39:11 +0100

    helloworld

---
+++ my-node-app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set static folder for CSS, JS, images
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
